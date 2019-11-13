24e78e8cff4b9a80f3b2a8fd3c38149fba6c0e61
Lukas Erlinghagen <lukas.erlinghagen@kit.edu>
2019-11-13 19:40:03 +0100

    Added docker scripts for local testing

---
+++ launch_jekyll_serve.bat
@@ -0,0 +1,3 @@
+@echo off
+if not exist "%~dp0\_gemcache" mkdir %~dp0\_gemcache
+docker run --rm -v %~dp0:/srv/jekyll -v %~dp0\_gemcache:/usr/local/bundle -p 4000:4000 -it jekyll/jekyll:3.8.5 jekyll serve
